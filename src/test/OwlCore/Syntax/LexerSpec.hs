a52443924f95fb52bd6c66c07059e98920c1377c
Harley D. Eades III <harley.eades@gmail.com>
2022-07-29 08:17:30 -0400

    Removed the stack test framework.

--- src/test/OwlCore/Syntax/LexerSpec.hs
+++
@@ -1,9 +0,0 @@
-module OwlCore.Syntax.LexerSpec where
-
-import Test.Hspec
-
-spec :: Spec
-spec = do
-  describe "lexer" $ do
-    it "should test the lexer" $ do
-      pending
--- src/test/OwlCore/Syntax/ParserSpec.hs
+++
@@ -1,9 +0,0 @@
-module OwlCore.Syntax.ParserSpec where
-
-import Test.Hspec
-
-spec :: Spec
-spec = do
-  describe "parser" $ do
-    it "should test the parser using ASTTests" $ do
-      pending
